5561ad992a2a1c4c3ca960fc68c10bda12651ab4
ReoF777 <reonafuji@gmail.com>
2025-07-30 11:50:55 +0900

    更新

--- GPTMecabGenerator/Program.cs
+++ GPTMecabGenerator/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MeCab;
+
+var mecab = MeCabTagger.Create();  // 解析器のインスタンスを作成
+var sampleText = "これはテスト音声です。";  // サンプルテキスト
+var nodes = mecab.ParseToNodes(sampleText);  // 形態素解析を実行
+
+foreach(var node in nodes)
+{
+    Console.WriteLine($"{node.Surface} - {node.Feature}");
+}
+
